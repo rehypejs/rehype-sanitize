33ab431337ea764b30a03afaf2568cfe78836f18
Titus Wormer <tituswormer@gmail.com>
2021-08-01 22:48:31 +0200

    Refactor code-style

--- test.js
+++ test.js
@@ -3,34 +3,34 @@ import {rehype} from 'rehype'
 import merge from 'deepmerge'
 import rehypeSanitize, {defaultSchema} from './index.js'
 
-test('rehypeSanitize', function (t) {
+test('rehypeSanitize', (t) => {
   t.plan(2)
 
-  t.test('should work', function (st) {
-    var input = '<img onmouseover="alert(\'alpha\')">'
-    var output = '<img>'
+  t.test('should work', (st) => {
+    const input = '<img onmouseover="alert(\'alpha\')">'
+    const output = '<img>'
 
     st.plan(3)
 
     rehype()
       .use(rehypeSanitize)
-      .process(input, function (error, file) {
+      .process(input, (error, file) => {
         st.ifErr(error, 'shouldn’t fail')
         st.equal(file.messages.length, 0, 'shouldn’t warn')
         st.equal(String(file), String(output), 'should match')
       })
   })
 
-  t.test('options', function (st) {
-    var input =
+  t.test('options', (st) => {
+    const input =
       '<math><mi xlink:href="data:x,<script>alert(\'echo\')</script>"></mi></math>'
-    var output = '<math><mi></mi></math>'
+    const output = '<math><mi></mi></math>'
 
     st.plan(3)
 
     rehype()
       .use(rehypeSanitize, merge(defaultSchema, {tagNames: ['math', 'mi']}))
-      .process(input, function (error, file) {
+      .process(input, (error, file) => {
         st.ifErr(error, 'shouldn’t fail')
         st.equal(file.messages.length, 0, 'shouldn’t warn')
         st.equal(String(file), String(output), 'should match')
