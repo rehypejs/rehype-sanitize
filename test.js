cecabaeff8e6584d0fe98fb4267fdf626e29c0c4
Titus Wormer <tituswormer@gmail.com>
2023-08-26 13:07:54 +0200

    Change to use `exports`

--- test.js
+++ test.js
@@ -2,11 +2,11 @@ import assert from 'node:assert/strict'
 import test from 'node:test'
 import merge from 'deepmerge'
 import {rehype} from 'rehype'
-import rehypeSanitize, {defaultSchema} from './index.js'
+import rehypeSanitize, {defaultSchema} from 'rehype-sanitize'
 
 test('rehypeSanitize', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('rehype-sanitize')).sort(), [
       'default',
       'defaultSchema'
     ])
